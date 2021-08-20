e45313195d906bd47828f1e251086e3c0f97beb7
Natasha <natasha.aleksandrova@zonarsystems.com>
2021-08-20 08:01:02 -0700

    Update grid_printer.py

--- grid_printer.py
+++ grid_printer.py
@@ -18,3 +18,12 @@ def print_grid2(size):
 # PART 3
 def print_grid3(box_size, cell_size):
     pass
+
+# executes your grid functions below
+if __name__ == "__main__":
+    print_grid1()
+    
+    print_grid2(3)
+    
+    print_grid3(3, 4)
+    
